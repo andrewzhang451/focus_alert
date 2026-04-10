b3766e4809b7f17f850e46ff58da9a1bdcce264c
andrewzhang451 <zhangandrew2020@gmail.com>
2026-04-10 12:24:06 -0500

    initial commit; imported camera functionality to test mac camera

---
+++ focus_tracker.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+#basic command to open laptop camera
+cap = cv2.VideoCapture(0)
+
+while True:
+  
+  ret, frame = cap.read()
+  
+  if not ret:
+    break
+  
+  cv2.imshow("Live Camera", frame)
+  
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+  
+cap.release()
+cv2.destroyAllWindows()
