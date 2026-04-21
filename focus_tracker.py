5ac1905ee829a994e9fc5219ade1f0cdd6956fb2
andrewzhang451 <zhangandrew2020@gmail.com>
2026-04-21 10:03:55 -0500

    added head drop relative to eye to nose position. added quick small testing and still in testing stage

--- focus_tracker.py
+++ focus_tracker.py
@@ -4,13 +4,21 @@ import mediapipe as mp
 #go into the mediapipe module to grab the face_mesh module 
 mp_face_mesh = mp.solutions.face_mesh
 # face_mesh is a more detailed face detection compared to the facebox version
-face_mesh = mp_face_mesh.FaceMesh()
+
+face_mesh = mp_face_mesh.FaceMesh(
+  # added frame refresh
+  max_num_faces=1,
+  refine_landmarks=True,
+  min_detection_confidence=0.5,
+  min_tracking_confidence=0.5
+)
 
 
 #basic command to open laptop camera
 cap = cv2.VideoCapture(0)
 
 while True:
+  focus = False
   
   ret, frame = cap.read()
   if not ret:
@@ -20,18 +28,42 @@ while True:
   results = face_mesh.process(rgb) #ask mediapip to check if there is a face
   
   
-  focus = False
-  
-  if results.multi_face_landmarks:
-    focus = True
   
   if results.multi_face_landmarks:
     for face_landmarks in results.multi_face_landmarks:
       h,w,_ = frame.shape
       
-      eye_point = [33,133,159,145]
+      # give coordinates for (nose, left eye, right eye)
+      nose = face_landmarks.landmark[1]
+      left_eye = face_landmarks.landmark[33]
+      right_eye = face_landmarks.landmark[263]
       
-      for point_id in eye_point:
+      nose_y = nose.y
+      eye_avg_y = (left_eye.y + right_eye.y)/2
+      
+      # bigger difference means the nose drop further below the eyes == looking down!
+      head_drop = nose_y - eye_avg_y
+      
+      if head_drop > 0.18:
+        focus = False
+      else:
+        focus = True
+        
+      # for testing purpose
+      
+      cv2.putText(
+        frame,
+        f"head_drop: {head_drop:.3f}",
+        (30, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.7,
+        (255, 255, 255),
+        2
+      )
+      
+      eye_points = [33, 133, 159, 145, 362, 263, 386, 374]
+      
+      for point_id in eye_points:
         # shorten this whole function into "lm" for easier re-usability 
         lm = face_landmarks.landmark[point_id] 
         x = int(lm.x * w)
@@ -40,7 +72,7 @@ while True:
         cv2.circle(frame, (x,y), 3, (0,255,0), -1)
         
   if focus:
-    cv2.putText(frame,"YOU ARE FOCUS", (30,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame,"FOCUSED", (30,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
   else:
     cv2.putText(frame, "DISTRACTED", (30,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
   
